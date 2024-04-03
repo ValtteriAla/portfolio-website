fde24851976dd1b7a14320d44e12a7a6360ba1ef
ValtteriAla <valtteri.m.alastalo@edu.riveria.fi>
2024-04-03 14:31:30 +0300

    Testing a game in js

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+hire_me_button = document.getElementById("hire-me-btn")
+
+hire_me_button.addEventListener("click", on_click_hire_me)
+
+function on_click_hire_me(e) {
+    console.log(e)
+    window.alert("I AM HIRED!!!!")
+}
